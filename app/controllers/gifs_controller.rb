e77732996ba63e12ff745489506b88d98b2c4d11
Andrew Deitrick <andrew@Andrews-MacBook-Air.local>
2015-01-23 13:05:56 -0700

    example of mvc to display gifs

--- app/controllers/gifs_controller.rb
+++ app/controllers/gifs_controller.rb
@@ -1,5 +1,11 @@
 class GifsController < ApplicationController
   def index
+    p "HEY THIS IS THE CONTROLLER SPEAKING"
     @random_gif = Gif.get_random_gif
+    p "HEY THANKS MODEL, THIS IS THE CONTROLLER AGAIN"
+  end
+
+  def show
+    @gif = Gif.give_me_this_gif(params[:id])
   end
 end
--- app/models/gif.rb
+++ app/models/gif.rb
@@ -1,5 +1,10 @@
 class Gif
   def self.get_random_gif
+    p "HEY THIS IS THE MODEL SPEAKING"
     [1,2,3,4,5,6].sample
+  end
+
+  def self.give_me_this_gif(id)
+    return id
   end
 end
